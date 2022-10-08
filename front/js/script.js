85d021e6a7c0a83bb6c418fdac3f40af78f0b20f
guillaumecousin7 <106609547+guillaumecousin7@users.noreply.github.com>
2022-10-08 19:47:31 +0200

    recuperation commencé

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,28 @@
-fetch("http://localhost:3000/api/products")
+fetch("http://localhost:3000/api/products")/* recup du tableau*/
     .then((res) => res.json())
-    .then((data) => console.log(data))
-    /* recup du tableau*/
+    .then((data) => {
+        console.log(data)
+        return addProducts(data)
+    })
+      
 
-    let anchor = document.createElement("a")
-    anchor.href='http://localhost:3000/images/kanap01.jpeg'
+
+function addProducts(donnees){
+     const id = donnees[0]._id
+     const anchor = makeAnchor(id)
+     appendChildren(anchor)
+
+}
+
+function makeAnchor(id){
+    const anchor = document.createElement("a")
+    anchor.href = "./product.html?id=" + id
+    return anchor
+}
+
+function appendChildren(anchor){
+    const items = document.getElementById("items")
+     if (items){
+    items.appendChild(anchor)
+    
+}}
