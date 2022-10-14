a9ab20464ec094a481f4aa1a1f5951b3c738ccb0
guillaumecousin7 <106609547+guillaumecousin7@users.noreply.github.com>
2022-10-14 14:53:14 +0200

    recup des produits index fini

--- front/js/script.js
+++ front/js/script.js
@@ -1,28 +1,71 @@
-fetch("http://localhost:3000/api/products")/* recup du tableau*/
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data)
-        return addProducts(data)
-    })
-      
+fetch("http://localhost:3000/api/products") /* recup du tableau*/
+.then((res) => res.json())
+.then((data) => {
+    console.log(data)
+    return addProducts(data)
+})
 
 
-function addProducts(donnees){
-     const id = donnees[0]._id
-     const anchor = makeAnchor(id)
-     appendChildren(anchor)
+function addProducts(kanap) {
+  /*const _id = kanap[0]._id
+    const imageURL = kanap[0].imageURL
+    const altTxt = kanap[0].altTxt
+    const name = kanap[0].name
+    const description = kanap[0].description*/
 
+    
+    kanap.forEach(kanap => {
+        
+    
+    
+    const {_id, imageUrl, altTxt, name, description} = kanap 
+    const anchor = makeAnchor(_id)
+
+    const article = document.createElement("article")
+    const image = makeImage(imageUrl, altTxt)
+    const h3 = makeH3(name)
+    const p = makeParagraphe(description)
+
+    article.appendChild(image)
+    article.appendChild(h3)
+    article.appendChild(p)
+
+    appendArticleToAnchor(anchor, article)
+ });
 }
 
-function makeAnchor(id){
+function makeAnchor(id) {
     const anchor = document.createElement("a")
     anchor.href = "./product.html?id=" + id
     return anchor
 }
-
-function appendChildren(anchor){
+/* cree une balise article*/
+function appendArticleToAnchor(anchor, article) {
     const items = document.getElementById("items")
-     if (items){
-    items.appendChild(anchor)
-    
-}}
+    if (items) {
+        items.appendChild(anchor)
+        anchor.appendChild(article)
+
+    }
+}
+/* recup des images et texte*/
+function makeImage(_imageURL, _altTxt) {
+    const image = document.createElement("img")
+    image.src = _imageURL
+    image.alt = _altTxt
+    return image
+}
+
+
+function makeH3(name) {
+    const h3 = document.createElement("h3")
+    h3.textContent = name
+    h3.classList.add("productName")
+    return h3
+}
+function makeParagraphe(description) {
+    const p = document.createElement("p")
+    p.textContent = description
+    p.classList.add("productDescription")
+    return p
+}
