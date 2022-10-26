5d7cae7623deb6abe757e05b3d39ab38629fc4ec
guillaumecousin7 <106609547+guillaumecousin7@users.noreply.github.com>
2022-10-26 11:45:52 +0200

    pour travailler a distance

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,7 @@
 
 const cart = []/*tableau pour stocker les items du panier global*/
 
+
 retrieveItemsFromCache() /*recupere les items du cache*/
 cart.forEach((item) => displayItem(item)) /*pour chaque item du panier on affiche l'article*/
 
@@ -24,12 +25,12 @@ function displayItem(item){
     displayTotalQuantity() /*affichage de la quantité total*/
     displayTotalPrice() /*affichage du prix total*/
 }
-function displayTotalQuantity(){
+function displayTotalQuantity(){/*affichage de la quantité total*/
     const totalQuantity = document.querySelector("#totalQuantity")
     const total = cart.reduce((total, item) => total + item.quantity, 0)
     totalQuantity.textContent = total
 }
-function displayTotalPrice(){
+function displayTotalPrice(){/*affichage du prix total et calcul du prix*/
     const totalPrice = document.querySelector("#totalPrice")
     const total = cart.reduce((total, item) => total + item.price * item.quantity, 0)
     totalPrice.textContent = total 
@@ -48,24 +49,34 @@ function makeCartContent(item){
     return cartItemContent
    
 }
-function MakeSettings(item){
-    const settings = document.createElement("div")
-    settings.classList.add("cart__item__content__settings")
+function MakeSettings(item){/* creation des settings*/
+    const settings = document.createElement("div") /*creation de la div pour les settings*/
+    settings.classList.add("cart__item__content__settings") /*ajout de la class*/
 
-    addQuantityToSettings(settings, item)
-    addDeleteToSettings(settings)
-    return settings
+    addQuantityToSettings(settings, item)   /*ajout de la quantité dans les settings*/
+    addDeleteToSettings(settings, item) /*ajout du bouton supprimer dans les settings*/
+    return settings 
 }
-function addDeleteToSettings(settings){
+function addDeleteToSettings(settings, item){ /*ajout du bouton supprimer dans les settings*/
     const quantity = document.createElement("div")
     quantity.classList.add("cart__item__content__settings__delete")
+    quantity.addEventListener("click",() => deleteItem(item))/*ajout d'un event pour supprimer l'item*/
+
     const p = document.createElement("p")
     p.textContent = "Supprimer"
     quantity.appendChild(p)
     settings.appendChild(quantity)
 }
+function deleteItem(item){/*supprimer l'item*/
+   const itemToDelete = 
+   cart.findIndex((item) => item.id === item.id && item.color === item.color ) /*recupere l'item a supprimer*/
+   cart.splice(itemToDelete, 1) /*supprimer l'item*/
+   uptadePriceAndQuantity(item.id, 0, item) /*mise a jour du prix et de la quantité*/
+    localStorage.removeItem(item.id) /*supprimer l'item du cache*/
 
-function addQuantityToSettings(settings, item){
+}
+
+function addQuantityToSettings(settings, item){ /*ajout de la quantité dans les settings*/
     const quantity = document.createElement("div")
     quantity.classList.add("cart__item__content__settings__quantity")
     const p = document.createElement("p")
@@ -78,12 +89,28 @@ function addQuantityToSettings(settings, item){
     input.min = "1"
     input.max = "100"
     input.value = item.quantity
+    input.addEventListener("input", () => uptadePriceAndQuantity(item.id, input.value , item))/*ajout d'un event pour modifier la quantité*/
+
     quantity.appendChild(input)
     settings.appendChild(quantity)
 }
 
 
-function makeDescription(item){
+function uptadePriceAndQuantity(id, newValue, item){/*mise a jour du prix et de la quantité*/
+    const itemToUpdate = cart.find((item) => item.id === id) /*recupere l'item a modifier*/
+    itemToUpdate.quantity = Number(newValue) /*modifie la quantité*/
+    displayTotalQuantity() /*affiche la quantité total*/
+    displayTotalPrice() /*affiche le prix total*/
+    deleteItem(item) /*supprime l'item*/
+    saveNewDataToCache(itemToUpdate) /*sauvegarde les nouvelles données dans le cache / storage*/
+}
+function saveNewDataToCache(item){/*sauvegarde les nouvelles données dans le cache / storage*/
+const dataToSave = JSON.stringify(item) /*changer la valeur dans le storage*/
+
+localStorage.setItem(item.id, dataToSave)/*sauvegarde et supp les nouvelles données dans le cache / storage*/
+}
+
+function makeDescription(item){/*creation de la description*/
  const description = document.createElement("div")
     description.classList.add("cart__item__content__description")
 
@@ -103,11 +130,11 @@ function makeDescription(item){
     return description
 }
 
-function displayArticle(article){
+function displayArticle(article){/*affichage de l'article*/
     document.querySelector("#cart__items").appendChild(article)
 }
 
-function makeArticle(item){
+function makeArticle(item){/*creation de l'article*/
     const article = document.createElement("article")
     article.classList.add("cart__item")
     article.dataset.id = item.id /*id est l'id de l'objet et dataset pour recup les data */
@@ -115,12 +142,12 @@ function makeArticle(item){
     return article
 }
 
-function makeImageDiv(item){
+function makeImageDiv(item){/*creation de la div pour l'image*/
     const div = document.createElement("div")/*creation de div pour mettre l'img dedans*/
     div.classList.add("cart__item__img")
 
 
-    const image = document.createElement("img")
+    const image = document.createElement("img")/*creation de l'image*/
     image.src = item.imageUrl
     image.alt = item.altTxt
     div.appendChild(image)
